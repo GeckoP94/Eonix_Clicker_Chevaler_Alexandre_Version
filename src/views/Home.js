ce20b0a65c29bd4353203b516e2a6aade335eb66
GeckoP94 <chevalieralexandre1994@hotmail.com>
2024-08-04 18:15:04 +0200

    02b - Compteur 100% fonctionnel - utilisation de fonctions déclarées en amont et appelées dans le onClick + Supression du useEffect inutile pour le compte au final message supprimé

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,31 +1,29 @@
 
-import React, { useState, useEffect } from 'react';
-
+import React, { useState } from 'react';
 
 // Je garde la fonction le nom de la fonction Home, car déja correctement appelé dans l'ensemble du template kit
 function Home() {
   // Déclare une nouvelle variable d'état, que nous appellerons "compte"
   const [compte, setCompte] = useState(0);
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    if (compte < 0) {
-      setMessage('Hola jeune malandrin, le compteur est en négatif, ça ne va nieu');
-    } else {
-      setMessage('le compteur est en positif good job');
+  const incrementCount = () => {
+    setCompte(compte + 1);
+  };
+  
+  const decrementCount = () => {
+    if (compte > 0) {
+      setCompte(compte - 1);
     }
-  }, [compte]); // Le hook useEffect se déclenche lorsque la variable count change
+  };
   
   return (
     <div className='sectionCounter'>
     <h1>{compte}</h1>
-    <button onClick={() => setCompte(compte + 1)}>
+    <button onClick={incrementCount}>
     Incrémenter la valeur de compteur
     </button>
-    <button onClick={() => setCompte(compte - 1)}>
+    <button onClick={decrementCount}>
     Décrémenter la valeur du compteur
     </button>
-    <p>{message}</p>
     </div>
   );
 }
