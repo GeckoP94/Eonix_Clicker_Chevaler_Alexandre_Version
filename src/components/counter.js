2b272bdd69326f2de4959aa74bca03eb57b8533f
GeckoP94 <chevalieralexandre1994@hotmail.com>
2024-08-04 20:19:31 +0200

    03 - composant gérant le compteurs sorti de la homeview dans un coposant unique et appelé dans cette dernière + Design de l'application appliqué sur base du site EONIX - Reponsive OK - Reste Test unitaire + Build de l'applicaton.

---
+++ src/components/counter.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+// Je garde la fonction le nom de la fonction Home, car déja correctement appelé dans l'ensemble du template kit
+function Counter() {
+    // Déclare une nouvelle variable d'état, que nous appellerons "compte"
+    const [compte, setCompte] = useState(0);
+    const incrementCount = () => {
+      setCompte(compte + 1);
+    };
+    
+    const decrementCount = () => {
+      if (compte > 0) {
+        setCompte(compte - 1);
+      }
+    };
+    
+    return (
+      <div className='sectionCounter'>
+      <div id="superCompteur">{compte}</div>
+      <button onClick={incrementCount}>
+      Incrémenter la valeur de compteur
+      </button>
+      <button onClick={decrementCount}>
+      Décrémenter la valeur du compteur
+      </button>
+      </div>
+    );
+  }
+
+  export default Counter;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,29 +1,15 @@
 
-import React, { useState } from 'react';
+import React from 'react';
+import Counter from '../components/counter';
+import eonix from'../assets/img/eonix.svg';
+import '../style.css';
 
-// Je garde la fonction le nom de la fonction Home, car déja correctement appelé dans l'ensemble du template kit
 function Home() {
-  // Déclare une nouvelle variable d'état, que nous appellerons "compte"
-  const [compte, setCompte] = useState(0);
-  const incrementCount = () => {
-    setCompte(compte + 1);
-  };
-  
-  const decrementCount = () => {
-    if (compte > 0) {
-      setCompte(compte - 1);
-    }
-  };
-  
   return (
-    <div className='sectionCounter'>
-    <h1>{compte}</h1>
-    <button onClick={incrementCount}>
-    Incrémenter la valeur de compteur
-    </button>
-    <button onClick={decrementCount}>
-    Décrémenter la valeur du compteur
-    </button>
+    <div className="Home">
+    <img src={eonix} alt="logo Eonix"/>
+    <h1>Clic Counter - Eonix par Chevalier Alexandre</h1>
+    <Counter />
     </div>
   );
 }
